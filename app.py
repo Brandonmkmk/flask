2de8ba5cb1b90e27fc177e5bd01e8627c4891771
Adrianxx1 <triaxis12q@gmail.com>
2022-08-27 13:14:16 -0500

    noveno commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import sqlite3
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+from flask import Flask, render_template
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
