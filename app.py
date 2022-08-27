863f089770420df3598e56e44d995eaa3216b02d
Adrianxx1 <triaxis12q@gmail.com>
2022-08-27 13:15:52 -0500

    decimo commit

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
+    
+
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+
